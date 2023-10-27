1c37eead1b1671cc55b3e0cbae116460b3d8b127
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-10-27 14:18:03 -0300

    getMany

--- src/services/cars.services.ts
+++ src/services/cars.services.ts
@@ -3,7 +3,7 @@ import { ICar, TCreateBody, TUpdateBody } from "../interfaces/cars.interface";
 
 interface ICarsServices{
     create(body: TCreateBody): ICar;
-    //getMany(search?: string, year?: string): ICar[];
+    getMany(search?: string, year?: string): ICar[];
     //getOne(id: string): ICar;
     //delete(id: string): void;
     //update(body: TUpdateBody, id: string): ICar;
