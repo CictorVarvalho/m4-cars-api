dd32167fe39efa16f93e8685a504ad70873f82b5
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-10-27 09:38:39 -0300

    router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { json } from "express";
+import { carsRouter } from "./routes/cars.routes";
 
 export const app = express();
 
 app.use(json());
+
+app.use("/cars", carsRouter);
---
+++ src/routes/cars.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+export const carsRouter = Router();
+
+carsRouter.post("/")
+carsRouter.get("/")
+carsRouter.get("/:id")
+carsRouter.delete("/:id")
+carsRouter.patch("/:id")
